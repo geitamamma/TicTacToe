7dcb4a8e256a0a1b9b47294a8e89ff7f1be12bad
Elvar <elvar11@ru.is>
2013-11-22 23:47:12 +0000

    added the player class

---
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -0,0 +1,22 @@
+package is.ru.tictactoe;
+
+public class Player{
+	private String name;
+	private int playerNr;
+
+	public Player(String _name, int _playerNr){
+		name = _name;
+		playerNr = _playerNr;
+	}
+
+	public String GetName(){
+	   return name;
+	}
+}
+
+
+
+
+
+
+
