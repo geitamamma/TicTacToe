b66819a73f7924fd6bd4eab4e039dee82cc3474f
hannesg11 <hannesg11@ru.is>
2013-11-22 11:47:36 +0000

    Changed selenium test script files

--- bin/selenium_test.bat
+++ bin/selenium_test.bat
@@ -3,5 +3,5 @@ rem ***********************************************************************
 rem This bat file runs Selenium tests (uses failsafe plugin).
 rem ***********************************************************************
 
-SET STAGING_SERVER=http://localhost:4567
+SET STAGING_SERVER="http://localhost:4567"
 call mvn test-compile failsafe:integration-test
