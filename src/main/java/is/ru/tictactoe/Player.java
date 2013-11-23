2d9541975952e3d720159f4f95c0bcb3b5815b21
Elvar <elvar11@ru.is>
2013-11-23 00:29:25 +0000

    added exception handling

--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -1,16 +1,25 @@
 package is.ru.tictactoe;
 
+
 public class Player{
 	private String name;
 	private int playerNr;
 
-	public Player(String _name, int _playerNr){
+	public Player(String _name, int _playerNr) throws IllegalArgumentException{
+		if(_name.isEmpty())
+			throw new IllegalArgumentException("Please enter a name.");
+		if(_playerNr < 1 || _playerNr > 2)
+			throw new IllegalArgumentException("Player number must be between 1 and 2.");
 		name = _name;
 		playerNr = _playerNr;
 	}
 
 	public String GetName(){
 	   return name;
+	}
+
+	public int GetPlayerNr(){
+		return playerNr;
 	}
 }
 
